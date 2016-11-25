a3e7576d8a891192b11161b86749eeab15374300
Loay <loay@users.noreply.github.com>
2016-11-24 22:50:25 -0500

    Update imported code

--- test/imported.test.js
+++ test/imported.test.js
@@ -4,11 +4,17 @@
 // License text available at https://opensource.org/licenses/MIT
 
 'use strict';
+var semver = require('semver');
+var mysqlVersion;
+
 describe('mysql imported features', function() {
   before(function() {
     require('./init.js');
   });
-
+  var describeMysql57 = describe;
+  if (semver.gte(mysqlVersion, '5.7.0')) {
+    describeMysql57 = describe.skip;
+  }
   require('loopback-datasource-juggler/test/common.batch.js');
   require('loopback-datasource-juggler/test/include.test.js');
 });
--- test/persistence-hooks.test.js
+++ test/persistence-hooks.test.js
@@ -3,6 +3,12 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
+'use strict';
+var should = require('./init');
+var suite = require('loopback-datasource-juggler/test/persistence-hooks.suite.js');
+
+suite(global.getDataSource(), should);
+
 // 'use strict';
 // var should = require('./init');
 // var suite = require('loopback-datasource-juggler/test/persistence-hooks.suite.js');
@@ -11,12 +17,12 @@
 //   replaceOrCreateReportsNewInstance: true,
 // });
 
-'use strict';
-describe('persistence-hooks suite test', function() {
-  before(function() {
-    require('./init.js');
-  });
-
-  require('loopback-datasource-juggler/test/persistence-hooks.suite.js');
-  replaceOrCreateReportsNewInstance: true,
-});
+// 'use strict';
+// describe('persistence-hooks suite test', function() {
+//   before(function() {
+//     require('./init.js');
+//   });
+//
+//   require('loopback-datasource-juggler/test/persistence-hooks.suite.js');
+//   replaceOrCreateReportsNewInstance: true;
+// });
