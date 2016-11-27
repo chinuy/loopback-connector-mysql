2080c5903d0df56cb15e2bc3a4fd1a30d85c3486
Loay <loay@users.noreply.github.com>
2016-11-27 02:10:30 -0500

    Update DB credentials

--- test/init.js
+++ test/init.js
@@ -15,7 +15,7 @@ global.getConfig = function(options) {
   var dbConf = {
     host: process.env.MYSQL_HOST || config.host || 'localhost',
     port: process.env.MYSQL_PORT || config.port || 3306,
-    database: 'myapp_test',
+    database: process.env.DATABASE || config.DATABASE,
     username: process.env.MYSQL_USER || config.username,
     password: process.env.MYSQL_PASSWORD || config.password,
     createDatabase: true,
